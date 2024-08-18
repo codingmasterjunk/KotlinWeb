0a8252b9f239bfc0605abc2d451e9230f06fbee7
wnsghksdl11 <47809905+wnsghksdl11@users.noreply.github.com>
2024-08-18 16:59:54 +0900

    Jvm version 1.9.24 -> 1.9.21로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
-    id("org.springframework.boot") version "3.3.2"
-    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.junk"
