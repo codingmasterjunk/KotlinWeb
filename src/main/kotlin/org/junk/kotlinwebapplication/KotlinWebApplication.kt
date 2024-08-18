12ee611e989abdff1aa395e73c3ec78a6d794e39
wnsghksdl11 <47809905+wnsghksdl11@users.noreply.github.com>
2024-08-18 16:10:12 +0900

    SpringBoot by Kotlin base

---
+++ src/main/kotlin/org/junk/kotlinwebapplication/KotlinWebApplication.kt
@@ -0,0 +1,11 @@
+package org.junk.kotlinwebapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebApplication>(*args)
+}
---
+++ src/test/kotlin/org/junk/kotlinwebapplication/KotlinWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.junk.kotlinwebapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
